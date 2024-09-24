8b871867e427a572c7628cfd9a9fcc8a4a266ba7
hossain Piash <hossainpiash933@gmail.com>
2024-09-25 03:14:02 +0600

    button color manage

--- calculation.js
+++ calculation.js
@@ -1,4 +1,26 @@
 
+function blog(){
+    window.location.href="blog.html";
+}
+
+function changeColor(){
+    const button = document.getElementById("donation");
+    button.style.backgroundColor="#B4F461";
+    const button2 = document.getElementById("history");
+    button2.style.backgroundColor="white";
+
+
+}
+
+function changeColor2(){
+    const button = document.getElementById("history");
+    button.style.backgroundColor="#B4F461";
+    const button2 = document.getElementById("donation");
+    button2.style.backgroundColor="white";
+
+
+}
+
 function getInputValueBYId(id){
     const inputValue =document.getElementById(id).value ;
     const inputNumber = parseFloat(inputValue);
@@ -28,5 +50,24 @@ document.getElementById('donate-now').addEventListener('click',function(event){
 
     document.getElementById('my-balance').innerText=newmyBalance;
     document.getElementById('noakhali-balace').innerText=newnoakhaliBalace;
+
+})
+
+
+document.getElementById('donate-money').addEventListener('click',function(event){
+    event.preventDefault();
+    const quota = getInputValueBYId('quota');
+    const quotaBalace=getTextFieldById('quota-balace');
+    const myBalance = getTextFieldById('my-balance');
+
+
+    
+   const newquotaBalace=quotaBalace + quota;
+  const  newmyBalance=myBalance-quota;
+
+    
+
+    document.getElementById('my-balance').innerText=newmyBalance;
+    document.getElementById('quota-balace').innerText=newquotaBalace;
 
 })
